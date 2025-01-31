e97af565d5f750d575c6aa475e17cd674b9bb09d
seemywings <kevin.jayne@icloud.com>
2025-01-30 19:27:52 -0500

    add GetGeoData

--- GeoData.go
+++ GeoData.go
@@ -1,5 +1,12 @@
 package openWeather
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
 type GeoData []struct {
 	Name       string `json:"name"`
 	LocalNames struct {
@@ -152,111 +159,31 @@ type GeoData []struct {
 		Ka          string `json:"ka"`
 		Ur          string `json:"ur"`
 	} `json:"local_names"`
-	Lat        float64 `json:"lat"`
-	Lon        float64 `json:"lon"`
-	Country    string  `json:"country"`
-	State      string  `json:"state"`
-	Population int     `json:"population"`
-	Timezone   string  `json:"timezone"`
+	Lat     float64 `json:"lat"`
+	Lon     float64 `json:"lon"`
+	Country string  `json:"country"`
+	State   string  `json:"state"`
 }
 
-type OWMOneCallResponse struct {
-	Lat            float64 `json:"lat"`
-	Lon            float64 `json:"lon"`
-	Timezone       string  `json:"timezone"`
-	TimezoneOffset int     `json:"timezone_offset"`
-	Current        struct {
-		Dt         int     `json:"dt"`
-		Sunrise    int     `json:"sunrise"`
-		Sunset     int     `json:"sunset"`
-		Temp       float64 `json:"temp"`
-		FeelsLike  float64 `json:"feels_like"`
-		Pressure   int     `json:"pressure"`
-		Humidity   int     `json:"humidity"`
-		DewPoint   float64 `json:"dew_point"`
-		Uvi        float64 `json:"uvi"`
-		Clouds     int     `json:"clouds"`
-		Visibility int     `json:"visibility"`
-		WindSpeed  float64 `json:"wind_speed"`
-		WindDeg    int     `json:"wind_deg"`
-		WindGust   float64 `json:"wind_gust"`
-		Weather    []struct {
-			ID          int    `json:"id"`
-			Main        string `json:"main"`
-			Description string `json:"description"`
-			Icon        string `json:"icon"`
-		} `json:"weather"`
-	} `json:"current"`
-	Minutely []struct {
-		Dt            int `json:"dt"`
-		Precipitation int `json:"precipitation"`
-	} `json:"minutely"`
-	Hourly []struct {
-		Dt         int     `json:"dt"`
-		Temp       float64 `json:"temp"`
-		FeelsLike  float64 `json:"feels_like"`
-		Pressure   int     `json:"pressure"`
-		Humidity   int     `json:"humidity"`
-		DewPoint   float64 `json:"dew_point"`
-		Uvi        int     `json:"uvi"`
-		Clouds     int     `json:"clouds"`
-		Visibility int     `json:"visibility"`
-		WindSpeed  float64 `json:"wind_speed"`
-		WindDeg    int     `json:"wind_deg"`
-		WindGust   float64 `json:"wind_gust"`
-		Weather    []struct {
-			ID          int    `json:"id"`
-			Main        string `json:"main"`
-			Description string `json:"description"`
-			Icon        string `json:"icon"`
-		} `json:"weather"`
-		Pop float64 `json:"pop"`
-	} `json:"hourly"`
-	Daily []struct {
-		Dt        int     `json:"dt"`
-		Sunrise   int     `json:"sunrise"`
-		Sunset    int     `json:"sunset"`
-		Moonrise  int     `json:"moonrise"`
-		Moonset   int     `json:"moonset"`
-		MoonPhase float64 `json:"moon_phase"`
-		Summary   string  `json:"summary"`
-		Temp      struct {
-			Day   float64 `json:"day"`
-			Min   float64 `json:"min"`
-			Max   float64 `json:"max"`
-			Night float64 `json:"night"`
-			Eve   float64 `json:"eve"`
-			Morn  float64 `json:"morn"`
-		} `json:"temp"`
-		FeelsLike struct {
-			Day   float64 `json:"day"`
-			Night float64 `json:"night"`
-			Eve   float64 `json:"eve"`
-			Morn  float64 `json:"morn"`
-		} `json:"feels_like"`
-		Pressure  int     `json:"pressure"`
-		Humidity  int     `json:"humidity"`
-		DewPoint  float64 `json:"dew_point"`
-		WindSpeed float64 `json:"wind_speed"`
-		WindDeg   int     `json:"wind_deg"`
-		WindGust  float64 `json:"wind_gust"`
-		Weather   []struct {
-			ID          int    `json:"id"`
-			Main        string `json:"main"`
-			Description string `json:"description"`
-			Icon        string `json:"icon"`
-		} `json:"weather"`
-		Clouds int     `json:"clouds"`
-		Pop    float64 `json:"pop"`
-		Rain   float64 `json:"rain"`
-		Uvi    float64 `json:"uvi"`
-	} `json:"daily"`
-	Alerts []struct {
-		SenderName  string `json:"sender_name"`
-		Event       string `json:"event"`
-		Start       int    `json:"start"`
-		End         int    `json:"end"`
-		Description string `json:"description"`
-		Tags        []any  `json:"tags"`
-	} `json:"alerts"`
+// GetGeoData: Get GeoData from OpenWeatherMap API
+func GetGeoData(location string) (GeoData, error) {
+	encLocation := url.QueryEscape(location)
+	url := "http://api.openweathermap.org/geo/1.0/direct?q=" + encLocation + "&limit=1&appid=" + OPEN_WEATHER_MAP_API_KEY
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to get geo data: %s", resp.Status)
+	}
+
+	var geoData GeoData
+	if err := json.NewDecoder(resp.Body).Decode(&geoData); err != nil {
+		return nil, err
+	}
+
+	return geoData, nil
 }
--- openWeather.go
+++ openWeather.go
@@ -1,48 +1,22 @@
 package openWeather
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
 )
 
-var OPEN_WEATHER_MAP_API_KEY string
-
 const apiURL = "https://api.openweathermap.org/data/3.0/onecal"
 
-func Init() error {
-	OPEN_WEATHER_MAP_API_KEY = os.Getenv("OPEN_WEATHER_MAP_API_KEY")
+var OPEN_WEATHER_MAP_API_KEY string
 
+func getAPIKey() error {
+	OPEN_WEATHER_MAP_API_KEY = os.Getenv("OPEN_WEATHER_MAP_API_KEY")
 	if OPEN_WEATHER_MAP_API_KEY == "" {
 		return fmt.Errorf("OPEN_WEATHER_MAP_API_KEY is not set")
 	}
-
 	return nil
 }
 
 func Now() {
 	fmt.Println("wAether 🌤️ in you CLI")
-}
-
-// get lat and long from provided town, city or zip code
-func GetGeoData(location string) (GeoData, error) {
-	url := "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&limit=1&appid=" + OPEN_WEATHER_MAP_API_KEY
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to get geo data: %s", resp.Status)
-	}
-
-	var geoData GeoData
-	if err := json.NewDecoder(resp.Body).Decode(&geoData); err != nil {
-		return nil, err
-	}
-
-	return geoData, nil
 }
